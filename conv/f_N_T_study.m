1586e5cc0a6729a4368a41ac55524247a5d12d6d
whmrtm <whmrtm@gmail.com>
2017-07-24 16:20:43 -0400

    Parameter study

--- conv/f_N_T_study.m
+++ conv/f_N_T_study.m
@@ -1,76 +1,19 @@
 
-pm2 = (1-exp(-f'.^2*T)).^N - (1-exp(-T)).^N;
-T = 30.0;
-max_iter = 150;
+% %% Theoritical Value
 
-alphas = logspace(-2, 2, 100);
-fs = linspace(0, 1, 100);
-pms = zeros(100, 100);
-
-s1 = sin(wave_num.*x);
-
-for i = 1:100
-    for j = 1:100
-        alpha = alphas(j);
-        f = fs(i);
-        s2 = alpha.*sin(f.*wave_num*x);
-        signal = s1 + s2;
-        [IMFs, residule] = conv_emd(x, signal, k, T, max_iter, 1);
-        HFC = IMFs(1,:);
-
-        % pms(i,j) = norm(HFC - s1) ./ norm(s1);
-        pms(i,j) = norm(HFC - s1) ./ norm(s2);
-
-        fprintf('%i,%i\n',i,j);
-
-    end
-end
+% T = 15.0;
+% N = 300;
 
 
-[myAlpha, myF] = meshgrid(alphas, fs);
-
-figure;
-mesh(myAlpha, myF, pms);
-set(gca, 'XScale', 'log');
-set(get(gca,'XLabel'),'String','alpha');
-set(get(gca,'YLabel'),'String','f');
-set(get(gca,'ZLabel'),'String','Performance Measure');
-% zlim([0,100]);
-caxis([0,1]);
-
-colorbar();
-
-
-
-% %% Theoritical value
-
-% T = 10.0;
-% L = 400;
-% x = linspace(0,6,L);
-
-% % define the diffusivity
-% k = 1/(4*pi.^2);
-% % k = 1;
-
-
-
-% wave_num = 2.0.*pi;
 % alphas = logspace(-2, 2, 100);
 % fs = linspace(0, 1, 100);
 % pms = zeros(100, 100);
 
-% s1 = sin(wave_num.*x);
-
 % for i = 1:100
 %     for j = 1:100
-%         alpha = alphas(i);
-%         f = fs(j);
-%         HFC = alpha*exp(-100.*exp(-f.^2*;
-
-%         pms(i,j) = norm(HFC - s1) ./ norm(s1);
-
-%         fprintf('%i,%i\n',i,j);
-
+%         alpha = alphas(j);
+%         f = fs(i);
+%         pms(i,j) = abs(1 - alpha*(1-exp(-f.^2*T)).^N - (1-exp(-T)).^N) ./ alpha;
 %     end
 % end
 
@@ -83,10 +26,58 @@ colorbar();
 % set(get(gca,'XLabel'),'String','alpha');
 % set(get(gca,'YLabel'),'String','f');
 % set(get(gca,'ZLabel'),'String','Performance Measure');
-% zlim([0,1]);
+% % zlim([0,1]);
 % caxis([0,1]);
 
 % colorbar();
 
+%% Explore the relationship between cutoff freq and N,T
+
+% Ns = logspace(1, 8, 500);
+Ns = linspace(10, 10000, 500);
+Ts = linspace(0,50,500);
+
+f0s = zeros(500, 500);
+
+f = linspace(0,1,500);
+epsilon = 0.05;
+
+for i = 1:500
+    for j = 1:500
+        N = Ns(j);
+        T = Ts(i);
+        pm2 = abs(1 - (1-exp(-f.^2*T)).^N - (1-exp(-T)).^N);
+        f_ind = find(pm2 > epsilon);
+        % Cutoff Frequency
+        if (1-exp(-T)).^N < 1-5*epsilon || f(f_ind(1)) == 0
+            f0s(i,j) = NaN;
+        else
+            f0s(i,j) = f(f_ind(1));
+        end
+    end
+end
 
 
+[myN, myT] = meshgrid(Ns, Ts);
+
+figure;
+mesh(myN, myT, f0s);
+% set(gca, 'XScale', 'log');
+set(get(gca,'XLabel'),'String','N');
+set(get(gca,'YLabel'),'String','T');
+set(get(gca,'ZLabel'),'String','Cutoff Freq ratio f0');
+% zlim([0,1]);
+caxis([0,1]);
+
+colorbar();
+
+
+% %% Single Group test
+% T = 10;
+% N = 1000;
+% epsilon = 0.05;
+% f = linspace(0, 1, 200);
+% pm2 = abs(1 - (1-exp(-f.^2*T)).^N - (1-exp(-T)).^N);
+% f_ind = find(pm2 > epsilon);
+% (1-exp(-T)).^N
+% f0 = f(f_ind(1))
