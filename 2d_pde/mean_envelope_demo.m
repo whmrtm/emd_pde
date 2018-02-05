dd79393eb851f3047ed782e95887a396923cd075
whmrtm <whmrtm@gmail.com>
2018-02-05 17:22:23 -0500

    s

--- 2d_pde/mean_envelope_demo.m
+++ 2d_pde/mean_envelope_demo.m
@@ -1,13 +1,13 @@
-% signal = imread('./img/fishingboat.jpg');
-% signal = signal(:,:,1);
+signal = imread('./img/Raffia.tiff');
+signal = signal(:,:,1);
 
-sz = [800,800];  %size of image [y,x], in pixels
-[x,y]= meshgrid(linspace(1,5,sz(2)),linspace(0,1,sz(1)));
-img = sin(exp(x)).*(y.^3);
-% img = sin(exp(x));
-
+% sz = [800,800];  %size of image [y,x], in pixels
+% [x,y]= meshgrid(linspace(1,5,sz(2)),linspace(0,1,sz(1)));
 % img = sin(exp(x)).*(y.^3);
-signal = (img+1)*128;
+% % img = sin(exp(x));
+% 
+% % img = sin(exp(x)).*(y.^3);
+% signal = (img+1)*128;
 
 
 % Generate the signal
